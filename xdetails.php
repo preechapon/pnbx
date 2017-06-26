b725b9a1ef06de6181d1f2155e899157159e2ddf
preechapon <preechaponb@gmail.com>
2017-06-26 21:56:18 +0700

    Update xdetails.php

--- xdetails.php
+++ xdetails.php
@@ -1,3 +1,7 @@
 <?php
-  echo "<table border=1 bgcolor=green><tr><td>xxxxx</td><td>rrrrr</td></tr></tr><tr><td>2xxxxx</td><td>2rrrrr</td></tr></tr></table>"
+  //echo "<table border=1 bgcolor=green><tr><td>xxxxx</td><td>rrrrr</td></tr></tr><tr><td>2xxxxx</td><td>2rrrrr</td></tr></tr></table>"
+echo "OK";
+header( "location: https://enigmatic-fjord-20579.herokuapp.com/xdetails.php" );
+exit(0);
+
 ?>
