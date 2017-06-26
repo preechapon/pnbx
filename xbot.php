813f82583793abb3606d91ebe0384ab113673320
preechapon <preechaponb@gmail.com>
2017-06-26 23:45:03 +0700

    Update xbot.php

--- xbot.php
+++ xbot.php
@@ -15,16 +15,51 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
+			
+//
+// A very simple PHP example that sends a HTTP POST to a remote site
+//
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL,"http://202.29.80.36/bizapp/skf_store/line_order.php");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,
+	    "p9=". $text);
+            
+
+// in real life you should use something like:
+// curl_setopt($ch, CURLOPT_POSTFIELDS, 
+//          http_build_query(array('postvar1' => 'value1')));
+
+// receive server response ...
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$server_output = curl_exec ($ch);
+
+curl_close ($ch);
+
+// further processing ....
+if ($server_output == "OK") { 
+	$xMsg="OK";
+} else {
+	$xMsg="No";
+}
+
+			
+			
+			
+			
 			//echo "OK";
 			//header( "http://202.29.80.36/bizapp/skf_store/line_order.php?p9=luser1+%2B+%E0%B8%82%E0%B8%B2%E0%B8%AB%E0%B8%A5%E0%B8%B1%E0%B8%87+15");
 			//exit(0);
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			// Build message to reply back
-			$xid=$event['userid'];
+			$xid=$events['userid'];
 			$messages = [
 				'type' => 'text',
-				'text' => $text . " rrrr x " . $xid . " ----- "
+				'text' => $xMsg . " ----- " . $text
 				//'type' => 'URL',
 				//'URL' => "http://202.29.80.36/bizapp/skf_store/"
 				
