ac7c1ce6e79e1bffc2441bec54ea095afb3ad6c9
preechapon <preechaponb@gmail.com>
2017-06-26 22:45:47 +0700

    Update xbot.php

--- xbot.php
+++ xbot.php
@@ -26,6 +26,12 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
+			echo "OK";
+			header( "location: http://202.29.80.36/bizapp/skf_store/line_order.php?p9=luser1 " . $text );
+			//exit(0);
+			
+			
+			
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			// Build message to reply back
