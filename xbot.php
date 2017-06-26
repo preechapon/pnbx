83d469968161a5c3766c6596179770854ab4d13d
preechapon <preechaponb@gmail.com>
2017-06-26 23:04:07 +0700

    Update xbot.php

--- xbot.php
+++ xbot.php
@@ -27,8 +27,8 @@ if (!is_null($events['events'])) {
 			// Get text sent
 			$text = $event['message']['text'];
 			echo "OK";
-			header( "location: http://202.29.80.36/bizapp/skf_store/line_order.php?p9=luser1 " . $text );
-			//exit(0);
+			header( "http://202.29.80.36/bizapp/skf_store/line_order.php?p9=luser1+%2B+%E0%B8%82%E0%B8%B2%E0%B8%AB%E0%B8%A5%E0%B8%B1%E0%B8%87+15");
+			exit(0);
 			
 			
 			
