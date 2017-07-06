6ae99ac655b24958773bb48a6c311a5727b05a7e
pon <preechaponb@gmail.com>
2017-07-06 20:31:02 +0700

    Update xbot.php

--- xbot.php
+++ xbot.php
@@ -54,7 +54,7 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
 */
   $strUrl = "https://api.line.me/v2/bot/message/push";
  
-  $arrPostData = array();
+  //$arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = " xxxxx "; 
