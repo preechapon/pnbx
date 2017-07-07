26843c7e72201a17d39862257530f5e7a1c8aae3
pon <preechaponb@gmail.com>
2017-07-07 23:33:50 +0700

    Update ordconfirm.php

--- ordconfirm.php
+++ ordconfirm.php
@@ -1,3 +1,3 @@
 <?php
-header( "location: http://202.29.80.36/bizapp/skf_store/line_order.php?p9=luser1+%E0%B8%A2%E0%B8%B7%E0%B8%99%E0%B8%A2%E0%B8%B1%E0%B8%99");
+header( "location: http://202.29.80.36/bizapp/skf/line_order.php?p9=luser1+%E0%B8%A2%E0%B8%B7%E0%B8%99%E0%B8%A2%E0%B8%B1%E0%B8%99");
 ?>
