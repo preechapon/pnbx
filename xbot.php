88f313d0e45188d9dde2e24ffec2a1668d5d43bd
pon <preechaponb@gmail.com>
2017-07-07 17:49:30 +0700

    Update xbot.php

--- xbot.php
+++ xbot.php
@@ -41,6 +41,7 @@ $ch = curl_init();
 	curl_setopt($ch, CURLOPT_POST, 1);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	$output = curl_exec($ch); 
 	curl_close($ch); 
 //echo $output;
