12afa64ae324de6b3ad8ccc185823275aea91363
pon <preechaponb@gmail.com>
2017-07-23 00:37:56 +0700

    Update showpic.php

--- showpic.php
+++ showpic.php
@@ -1,5 +1,5 @@
 <?php
- echo "<H1> ยินดีต้อนรับครับ </H1>";
- echo "<img src='http://202.29.80.36/bizapp/skf/asset/images/29Query.png' width=100% height=100% align='center'>" ;
+ echo "<center><font size=5> ยินดีต้อนรับครับ</font>  </center>";
+ echo "<img src='http://202.29.80.36/bizapp/skf/asset/images/29Query.png' width=65% height=65% align='center'>" ;
  //exit(0);
 ?>
