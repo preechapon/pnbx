90578bf96067de7ee4e3e9a8e38451368bf59f0a
pon <preechaponb@gmail.com>
2017-07-23 00:26:15 +0700

    Update showpic.php

--- showpic.php
+++ showpic.php
@@ -1,4 +1,4 @@
 <?php
- echo <img src="http://202.29.80.36/bizapp/skf/asset/images/product/sample_product_01.jpg"> ;
+ echo "<img src='http://202.29.80.36/bizapp/skf/asset/images/product/sample_product_01.jpg'>" ;
  //exit(0);
 ?>
